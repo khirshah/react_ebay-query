03239dce3eac6c251d37a9b41156695da471dbc6
khirshah <khirshah@gmail.com>
2019-08-21 15:48:10 +0100

    styling, making result rows look nicer, changing font and spacing of buttons

--- app/components/ResultRow.js
+++ app/components/ResultRow.js
@@ -11,11 +11,13 @@ export default class ResultRow extends Component{
   render() {
     return (
       <div className={styles.resultRow} >
-        <div className={styles.titleContainer}>
-          <a className={styles.title} href={this.props.content.ref} target="_blank" rel="noopener noreferrer">{this.props.content.title}</a>
+        <div className={styles.imgContainer} >
+          <img src={this.props.content.image.imageUrl} />
         </div>
         <div className={styles.details}>
-          <img src={this.props.content.image.imageUrl} />
+          <div className={styles.titleContainer}>
+            <a className={styles.title} href={this.props.content.ref} target="_blank" rel="noopener noreferrer">{this.props.content.title}</a>
+          </div>
           <div className={styles.infoBox}>
             <div className={styles.price}>{`Price: ${this.props.content.price.value} ${this.props.content.price.currency}`}</div>
             <div className={styles.location}>Country: {this.props.content.loc.country}</div>
